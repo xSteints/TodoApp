4277f3dc87e9ffa3670c4d950553abd5a902392a
surya <suryaputra0702@gmail.com>
2022-05-24 09:48:36 +0700

    todo list

--- lib/main.dart
+++ lib/main.dart
@@ -32,9 +32,12 @@ class MyHomePage extends StatelessWidget {
     return Scaffold(
       bottomNavigationBar: FloatingActionButton(
         onPressed: () {
+          // When the User clicks on the button, display a BottomSheet
           showModalBottomSheet(
             context: context,
-            builder: showBottomSheet,
+            builder: (context) {
+              return showBottomSheet(context, false, null);
+            },
           );
         },
         child: const Icon(Icons.add),
@@ -43,42 +46,94 @@ class MyHomePage extends StatelessWidget {
         title: const Text('Todo App'),
         centerTitle: true,
       ),
-      body: Text("Our Todo's will be here"),
+      body: StreamBuilder(
+        // Reading Items form our Database Using the StreamBuilder widget
+        stream: db.collection('todos').snapshots(),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if (!snapshot.hasData) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          return ListView.builder(
+            itemCount: snapshot.data?.docs.length,
+            itemBuilder: (context, int index) {
+              DocumentSnapshot documentSnapshot = snapshot.data.docs[index];
+              return ListTile(
+                title: Text(documentSnapshot['todo']),
+                onTap: () {
+                  // Here We Will Add The Update Feature and passed the value 'true' to the is update
+                  // feature.
+                  showModalBottomSheet(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return showBottomSheet(context, true, documentSnapshot);
+                    },
+                  );
+                },
+                trailing: IconButton(
+                  icon: const Icon(
+                    Icons.delete_outline,
+                  ),
+                  onPressed: () {
+                    // Here We Will Add The Delete Feature
+                    db.collection('todos').doc(documentSnapshot.id).delete();
+                  },
+                ),
+              );
+            },
+          );
+        },
+      ),
     );
   }
 }
 
-Widget showBottomSheet(BuildContext context) {
+showBottomSheet(
+    BuildContext context, bool isUpdate, DocumentSnapshot? documentSnapshot) {
+  // Added the isUpdate argument to check if our item has been updated
   return Padding(
     padding: const EdgeInsets.only(top: 20),
     child: Column(
-      // mainAxisAlignment: MainAxisAlignment.center,
       children: [
         SizedBox(
           width: MediaQuery.of(context).size.width * 0.9,
           child: TextField(
-            decoration: const InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'Add Todo',
+            decoration: InputDecoration(
+              border: const OutlineInputBorder(),
+              // Used a ternary operator to check if isUpdate is true then display
+              // Update Todo.
+              labelText: isUpdate ? 'Update Todo' : 'Add Todo',
               hintText: 'Enter An Item',
             ),
             onChanged: (String _val) {
+              // Storing the value of the text entered in the variable value.
               value = _val;
             },
           ),
         ),
         TextButton(
             style: ButtonStyle(
-                backgroundColor:
-                    MaterialStateProperty.all(Colors.lightBlueAccent)),
+              backgroundColor:
+                  MaterialStateProperty.all(Colors.lightBlueAccent),
+            ),
             onPressed: () {
-              db.collection('todos').add({'todo': value});
+              // Check to see if isUpdate is true then update the value else add the value
+              if (isUpdate) {
+                db.collection('todos').doc(documentSnapshot?.id).update({
+                  'todo': value,
+                });
+              } else {
+                db.collection('todos').add({'todo': value});
+              }
               Navigator.pop(context);
             },
-            child: const Text(
-              'ADD',
-              style: TextStyle(color: Colors.white),
-            ))
+            child: isUpdate
+                ? const Text(
+                    'UPDATE',
+                    style: TextStyle(color: Colors.white),
+                  )
+                : const Text('ADD', style: TextStyle(color: Colors.white))),
       ],
     ),
   );
